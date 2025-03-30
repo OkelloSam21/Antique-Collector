e25958e44740b9317f11ed4154143f4ce711617d
okello <samuelokello929@gmail.com>
2025-03-30 19:44:12 +0300

    Add Extended Icons and debug database initialization
    - Added extended material icons dependency.
    - Logged database initialization count and path in MainActivity for debugging.

--- app/src/main/java/com/example/antiquecollector/MainActivity.kt
+++ app/src/main/java/com/example/antiquecollector/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.antiquecollector
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,10 +9,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
+import com.example.antiquecollector.data.local.AppDatabase
 import com.example.antiquecollector.ui.screens.onboarding.OnboardingPreferences
 import com.example.antiquecollector.ui.theme.AntiqueCollectorTheme
 import com.example.antiquecollector.util.SplashScreenManager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -24,11 +29,23 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var onboardingPreferences: OnboardingPreferences
 
+    @Inject
+    lateinit var dataBase: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Initialize SplashScreenManager
         SplashScreenManager(this, onboardingPreferences, lifecycleScope)
+
+        lifecycleScope.launch (Dispatchers.IO){
+            try {
+                val count = dataBase.categoryDao().getCategoryCount().first()
+                Log.d("MainActivity", "Database initialized count: $count")
+            } catch (e:Exception) {
+                Log.d("MainActivity", "Error initializing database")
+            }
+        }
 
         setContent {
             AntiqueCollectorTheme {
@@ -40,5 +57,8 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        val dbPath  = applicationContext.getDatabasePath("antique_collector_database").absolutePath
+        Log.d("MainActivity", "Database path : $dbPath")
     }
 }
